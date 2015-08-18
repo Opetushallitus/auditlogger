a3eb03b78c784321faa250b21ea10b718021cc0b
Jussi Vesala <jussi.vesala@reaktor.fi>
2015-08-18 13:37:11 +0300

    add haku-loggin-package

---
+++ src/main/java/fi/vm/sade/auditlog/haku/HakuMessageFields.java
@@ -0,0 +1,9 @@
+package fi.vm.sade.auditlog.haku;
+
+public class HakuMessageFields {
+    public static final String HAKEMUSOID = "hakemusOid";
+    public static final String HAKUOID = "hakuOid";
+    public static final String HAKUKOHDEOID = "hakukohdeOid";
+
+    public static final String OPERAATIO  = "operaatio";
+}
