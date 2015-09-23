7c5f4b12e6c43517dbc95cdd5508de7df8e95524
Pasi Kallioniemi <pasi.kallioniemi@gofore.com>
2015-09-23 10:25:11 +0300

    Added themequestion operations to haku audit

--- src/main/java/fi/vm/sade/auditlog/haku/HakuMessageFields.java
+++ src/main/java/fi/vm/sade/auditlog/haku/HakuMessageFields.java
@@ -5,4 +5,5 @@ public class HakuMessageFields {
     public static final String HAKUOID = "hakuOid";
     public static final String HAKUKOHDEOID = "hakukohdeOid";
     public static final String HAKUKOHDERYHMAOID = "hakukohderyhmaOid";
+
 }
