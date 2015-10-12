b3f3b37b9c4f7d5686f8d819399aae012fcb79f0
Tomi Katajanheimo <tomi.katajanheimo@gmail.com>
2015-10-12 15:45:05 +0300

    add valintarekisteri audit logger

---
+++ src/main/java/fi/vm/sade/auditlog/valintarekisteri/ValintarekisteriFields.java
@@ -0,0 +1,8 @@
+package fi.vm.sade.auditlog.valintarekisteri;
+
+public class ValintarekisteriFields {
+    public static final String KOHDEHENKILO_OID = "kohdeHenkiloOid";
+    public static final String HAKUKOHDE_OID = "hakukohdeOid";
+    public static final String ILMOITTAJAHENKILO_OID = "ilmoittajaHenkiloOid";
+    public static final String ILMOITUSHETKI = "ilmoitushetki";
+}
---
+++ src/main/java/fi/vm/sade/auditlog/valintarekisteri/ValintarekisteriOperation.java
@@ -0,0 +1,5 @@
+package fi.vm.sade.auditlog.valintarekisteri;
+
+public enum ValintarekisteriOperation {
+    VASTAANOTTO_TALLENNUS, VASTAANOTTO_POISTO
+}
