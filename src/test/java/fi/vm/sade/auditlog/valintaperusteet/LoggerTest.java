5179aa3a71e2286cbb49c84692980349c2049287
Juha Paananen <juha.paananen@gmail.com>
2015-12-08 15:06:15 +0200

    make test more explicit

--- src/test/java/fi/vm/sade/auditlog/valintaperusteet/LoggerTest.java
+++ src/test/java/fi/vm/sade/auditlog/valintaperusteet/LoggerTest.java
@@ -1,25 +1,29 @@
 package fi.vm.sade.auditlog.valintaperusteet;
 
-import com.google.common.collect.ImmutableMap;
-import com.google.common.reflect.TypeToken;
-import com.google.gson.Gson;
-import fi.vm.sade.auditlog.ApplicationType;
-import fi.vm.sade.auditlog.Audit;
-import fi.vm.sade.auditlog.valintaperusteet.LogMessage;
-import org.junit.Assert;
+import static fi.vm.sade.auditlog.valintaperusteet.LogMessage.builder;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import java.io.File;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Map;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
 import org.slf4j.Logger;
 
-import java.io.File;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Map;
+import com.google.common.reflect.TypeToken;
+import com.google.gson.Gson;
 
-import static org.junit.Assert.*;
-import static org.mockito.Mockito.*;
-import static fi.vm.sade.auditlog.valintaperusteet.LogMessage.builder;
+import fi.vm.sade.auditlog.ApplicationType;
+import fi.vm.sade.auditlog.Audit;
 
 public class LoggerTest {
     private enum TESTENUM {
@@ -36,24 +40,13 @@ public class LoggerTest {
     }
 
     @Test
-    public void smokeTest() {
-        Date now = new Date();
-        audit.log(builder()
-                .id("testuser")
-                .timestamp(now)
-                .add("tila", TESTENUM.TILA1, TESTENUM.TILA2)
-                .message("test message")
-                .build());
+    public void smokeTest() throws ParseException {
+        Date now = new SimpleDateFormat("yyyy-MM-dd").parse("2015-12-01");
+        audit.log(builder().id("testuser").timestamp(now).add("tila", TESTENUM.TILA1, TESTENUM.TILA2).message("test message").build());
         ArgumentCaptor<String> infoCapture = ArgumentCaptor.forClass(String.class);
         verify(loggerMock, times(1)).info(infoCapture.capture());
-        Assert.assertEquals(jsonToMap(infoCapture.getValue()), ImmutableMap.builder()
-                .put("id","testuser")
-                .put("timestamp",SDF.format(now))
-                .put("message","test message")
-                .put("tila.old_value","TILA2")
-                .put("tila","TILA1")
-                .put("serviceName","test")
-                .put("applicationType","opiskelija").build());
+        final String logMessage = infoCapture.getValue();
+        assertEquals("{\"timestamp\":\"2015-12-01 00:00:00.000\",\"serviceName\":\"test\",\"applicationType\":\"opiskelija\",\"tila.old_value\":\"TILA2\",\"id\":\"testuser\",\"tila\":\"TILA1\",\"message\":\"test message\"}", logMessage);
     }
 
     @Test
