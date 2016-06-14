ebd9c946b56fc06426f41893b03314cb7f29c946
Tomi Katajanheimo <tomi.katajanheimo@gmail.com>
2016-06-14 11:55:22 +0300

    Add new operation for hakurekisteri

--- src/main/java/fi/vm/sade/auditlog/hakurekisteri/HakuRekisteriOperation.java
+++ src/main/java/fi/vm/sade/auditlog/hakurekisteri/HakuRekisteriOperation.java
@@ -1,5 +1,5 @@
 package fi.vm.sade.auditlog.hakurekisteri;
 
 public enum HakuRekisteriOperation {
-    RESOURCE_DELETE, RESOURCE_CREATE, RESOURCE_UPDATE
+    RESOURCE_DELETE, RESOURCE_CREATE, RESOURCE_UPDATE, READ_VIRTA_TIEDOT
 }
