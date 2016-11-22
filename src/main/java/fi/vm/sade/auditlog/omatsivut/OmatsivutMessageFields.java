8c17d9282c6dbb742414f65534ee4540265749a6
jussi.jartamo@gofore.com <jussi.jartamo@gofore.com>
2016-11-22 13:18:36 +0200

    omatsivut fields added

---
+++ src/main/java/fi/vm/sade/auditlog/omatsivut/OmatsivutMessageFields.java
@@ -0,0 +1,8 @@
+package fi.vm.sade.auditlog.omatsivut;
+
+public class OmatsivutMessageFields {
+    public static final String USER_OID = "userOid";
+    public static final String HAKU_OID = "hakuOid";
+    public static final String HAKUKOHDE_OID = "hakukohdeOid";
+    public static final String HAKEMUS_OID = "hakemusOid";
+}
