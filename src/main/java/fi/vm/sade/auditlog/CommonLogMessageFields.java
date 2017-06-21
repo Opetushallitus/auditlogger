9c86993a414de08128e2e31616df729605df3ab1
Joni Mäkelä <joni.makela@gmail.com>
2017-06-21 14:35:29 +0300

    Added ability to save nested map structures
    messageJson saves nested maps into message field using gson to convert
    the nested map into a string. ChangesJson does the same but saves into
    the changes field.

--- src/main/java/fi/vm/sade/auditlog/CommonLogMessageFields.java
+++ src/main/java/fi/vm/sade/auditlog/CommonLogMessageFields.java
@@ -6,4 +6,9 @@ public class CommonLogMessageFields {
     public static final String MESSAGE = "message";
     public static final String TIMESTAMP = "timestamp";
     public static final String OPERAATIO  = "operaatio";
+
+    /**
+     * In english as per docs say
+     */
+    public static final String CHANGES = "changes";
 }
